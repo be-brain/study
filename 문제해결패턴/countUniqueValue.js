5e14a4e73474867aad5a1b30d163963caa7f5875
be-brain <chha5232@gmail.com>
2023-05-09 23:56:15 +0900

    feat: 고유값 세기 풀이추가

--- 문제해결패턴/countUniqueValue.js
+++ 문제해결패턴/countUniqueValue.js
@@ -16,3 +16,16 @@ function countUniqueValues(arr) {
 }
 
 console.log(countUniqueValues(arr));
+
+// 정답
+function countUniqueValues2(arr) {
+    let i = 0;
+    if (arr.length === 0) return 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}

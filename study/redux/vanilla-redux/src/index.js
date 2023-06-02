40932fc47387cc4b728bc6d0d6a29ee62a509ee6
be-brain <chha5232@gmail.com>
2023-06-02 21:13:38 +0900

    feat : 계산기(바닐라js vs Pure Redux)

--- study/redux/vanilla-redux/src/index.js
+++ study/redux/vanilla-redux/src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const { createStore } = require("redux");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// let count = 0;
+// number.innerText = count;
+
+// const updateText = () => {
+//     number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//     count = count + 1;
+//     updateText();
+// };
+// const handleMinus = () => {
+//     count = count - 1;
+//     updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countReducer = (count = 0, action) => {
+    console.log(count, action);
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            break;
+    }
+    return count;
+};
+const countStore = createStore(countReducer);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+// subscribe = state의 변화를 알수있게 해준다
+
+const handleAdd = () => {
+    countStore.dispatch({ type: ADD });
+};
+const handleMinus = () => {
+    countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);

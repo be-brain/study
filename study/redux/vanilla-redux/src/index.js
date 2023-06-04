75344d1eaad5c5f49c1948c9f3e600afd3bd5224
be-brain <chha5232@gmail.com>
2023-06-05 00:24:53 +0900

    feat : updateTodo(vanillajs)

--- study/redux/vanilla-redux/src/index.js
+++ study/redux/vanilla-redux/src/index.js
@@ -10,14 +10,20 @@ const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const REMOVE_TODO = "REMOVE_TODO";
+const UPDATE_TODO = "UPDATE_TODO";
 
 const toDoReducer = (state = [], action) => {
-    console.log(state, action);
     switch (action.type) {
         case ADD_TODO:
             return [{ text: action.text, id: Date.now() }, ...state];
         case REMOVE_TODO:
             return state.filter((item) => item.id !== action.id);
+        // case UPDATE_TODO:
+        //     return state.map((item) => {
+        //         if (item.id === action.id) {
+        //             return [{ text: action.txt }, ...state];
+        //         }
+        //     });
         default:
             return state;
     }
@@ -37,12 +43,16 @@ const createTodo = () => {
     toDos.forEach((element) => {
         const li = document.createElement("li");
         const btn = document.createElement("button");
+        const editBtn = document.createElement("button");
         btn.innerHTML = "DEL";
         btn.addEventListener("click", deleteTodo);
+        editBtn.innerHTML = "EDT";
+        editBtn.addEventListener("click", editTodo);
         li.id = element.id;
         li.innerText = element.text;
         ul.appendChild(li);
         li.appendChild(btn);
+        li.appendChild(editBtn);
     });
 };
 
@@ -51,6 +61,53 @@ toDoStore.subscribe(createTodo);
 const deleteTodo = (e) => {
     const id = parseInt(e.target.parentNode.id);
     return toDoStore.dispatch({ type: REMOVE_TODO, id });
+};
+/* 
+    수정버튼누르면 input폼과 수정완료/취소버튼
+    default값이 들어있는 input폼 띄우기
+
+    1. 수정완료일때
+    : input값으로 기존li를 바꿔준다
+    2. 취소일때
+    : 그냥 창을 닫는다
+*/
+const editTodo = (e) => {
+    const todoItem = e.target.parentNode;
+    const todoItemText = todoItem.firstChild;
+    const inputText = todoItemText.textContent;
+
+    const divElem = document.createElement("div");
+    const inputElem = document.createElement("input");
+    inputElem.value = inputText;
+
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = "OK";
+    completeBtn.addEventListener("click", updateTodo);
+
+    const cancelBtn = document.createElement("button");
+    cancelBtn.innerHTML = "CANCEL";
+    cancelBtn.addEventListener("click", cancel);
+
+    todoItem.appendChild(divElem);
+    divElem.appendChild(inputElem);
+    divElem.appendChild(completeBtn);
+    divElem.appendChild(cancelBtn);
+};
+
+const cancel = (e) => {
+    const ele = e.target.parentNode;
+    ele.style.display = "none";
+};
+
+const updateTodo = (e) => {
+    const editBox = e.target.parentNode;
+    const updateInput = editBox.firstChild;
+    const id = parseInt(editBox.parentNode.id);
+
+    const originInput = editBox.parentNode.firstChild;
+    originInput.textContent = updateInput.value;
+
+    editBox.style.display = "none";
 };
 
 // const onSubmit = (e) => {

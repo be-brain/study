d06b5027cb547ebc221563d0713575af0400da90
Chaeha Kim <chha5232@gmail.com>
2023-10-07 16:43:03 +0900

    feat : formData, mock api를 이용한 로그인구현

--- study/onboarding-10-fe-p1/src/functions/getFormValues.ts
+++ study/onboarding-10-fe-p1/src/functions/getFormValues.ts
@@ -2,6 +2,7 @@ const getFormValues = (form: HTMLFormElement) => {
     const formData = new FormData(form);
     // const username = formData.get("username");
     // const password = formData.get("password");
+    // for (let [key, value] of formData.entries()) console.log(key, value);
     const values = [...formData.values()];
     const isEmpty = values.includes("");
     const data = Object.fromEntries(formData);
--- study/onboarding-10-fe-p1/src/pages/LoginWithMockAPI.tsx
+++ study/onboarding-10-fe-p1/src/pages/LoginWithMockAPI.tsx
@@ -35,25 +35,28 @@ const users: User[] = [
 const token = "success!@#$";
 
 const login = async (
-    username: FormDataEntryValue,
-    password: FormDataEntryValue
+    username: string,
+    password: string
 ): Promise<LoginResponse | null> => {
     // TODO: 올바른 username, password를 입력하면 {message: 'SUCCESS', token: (원하는 문자열)} 를 반환하세요.
-    const isMember = users.find(
-        (item) => item.name === username && item.pw === password
+    const member: User | undefined = users.find(
+        (user) => user.name === username && user.pw === password
     );
-    return isMember ? { message: "SUCCESS", token } : null;
+    return member
+        ? { message: "SUCCESS", token: JSON.stringify({ token, member }) }
+        : null;
 };
 
 const getUserInfo = async (userInfo: any): Promise<{ info: string } | null> => {
     // TODO: login 함수에서 받은 token을 이용해 사용자 정보를 받아오세요.
-
-    return null;
+    const info = JSON.parse(userInfo.token).member;
+    return userInfo ? info : null;
 };
 
 const LoginWithMockAPI = () => {
     const [isEmpty, setIsEmpty] = useState(false);
-    const [isMember, setIsMember] = useState(false);
+    const [userInfo, setUserInfo] = useState("");
+
     const loginSubmitHandler = async (
         event: React.FormEvent<HTMLFormElement>
     ) => {
@@ -61,9 +64,15 @@ const LoginWithMockAPI = () => {
         // TODO: form 에서 username과 password를 받아 login 함수를 호출하세요.
         const { isEmpty, data } = getFormValues(event.currentTarget);
         isEmpty ? setIsEmpty(true) : setIsEmpty(false);
-        getUserInfo(login(data.username, data.password));
-        // for (let [key, value] of formData.entries()) console.log(key, value);
         event.currentTarget.reset();
+
+        const loginResult = await login(
+            data.username as string,
+            data.password as string
+        );
+        if (!loginResult) return;
+        const infoResult = await getUserInfo(loginResult);
+        return infoResult ? setUserInfo(infoResult.info) : undefined;
     };
 
     return (
@@ -73,7 +82,7 @@ const LoginWithMockAPI = () => {
                 {/* TODO: 여기에 username과 password를 입력하는 input을 추가하세요. 제출을 위해 button도 추가하세요. */}
                 <label htmlFor="username">Username:</label>
                 <input type="text" id="username" name="username" />
-                {/* input에 반드시 name이 있어야 한다 */}
+                {/* formData를 사용할때는 input에 반드시 name속성이 있어야 한다 */}
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" />
                 <button style={{ border: "none" }}>Submit</button>
@@ -84,7 +93,7 @@ const LoginWithMockAPI = () => {
                 )}
                 <h2>User info</h2>
                 {/* TODO: 유저 정보를 보여주도록 구현하세요. 필요에 따라 state나 다른 변수를 추가하세요. */}
-                {/* <p>{JSON.stringify({ username: "blue" })}</p> */}
+                {userInfo && <h5 style={{ color: "blue" }}>{userInfo}</h5>}
             </div>
         </div>
     );

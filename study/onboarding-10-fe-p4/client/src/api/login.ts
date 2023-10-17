9d2024253d1e7749dbb2e6e136bbb46054eef36e
be-brain <chha5232@gmail.com>
2023-10-18 04:18:12 +0900

    feat : admin 라우터추가, admin권한에 따른 sidebar노출여부 함수구현

---
+++ study/onboarding-10-fe-p4/client/src/api/login.ts
@@ -0,0 +1,63 @@
+import { Item, User } from "../types/user";
+import { BASE_URL } from "./const";
+
+type LoginResult = "success" | "fail";
+
+export interface LoginRequest {
+    username: string;
+    password: string;
+}
+
+export const login = async (args: LoginRequest): Promise<LoginResult> => {
+    const loginRes = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            credentials: "include",
+        },
+        body: JSON.stringify(args),
+    });
+
+    return loginRes.ok ? "success" : "fail";
+};
+
+export const getCurrentUserInfo = async (): Promise<User | null> => {
+    const userInfoRes = await fetch(`${BASE_URL}/profile`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            credentials: "include",
+        },
+    });
+
+    return userInfoRes.ok ? userInfoRes.json() : null;
+};
+
+// TODO 4-2: GET, '/items' 호출
+export const getItems = async (): Promise<Item[] | null> => {
+    return null;
+};
+
+// TODO 4-2: GET, '/all-items' 호출
+export const getAllItems = async (): Promise<Item[] | null> => {
+    return null;
+};
+
+// TODO 4-2: POST, '/logout' 호출
+export const logout = async (): Promise<void> => {
+    const logoutRes = await fetch(`${BASE_URL}/logout`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            credentials: "include",
+        },
+    });
+
+    return logoutRes.json();
+    // .json()붙인것과 안붙인것 무슨차이?
+    /*
+    json()인터페이스의 메서드는 스트림을 Response가져와 Response완료될 때까지 읽습니다. 본문 텍스트를 구문 분석한 결과로 해결되는 Promise를 반환합니다.
+    fetch로는 데이터를 바로 사용할 수 없다. fetch를 사용할 땐 두 단계를 거쳐야 한다.
+    먼저 올바른 url로 요청을 보내야 하고, 바로 뒤에오는 응답에 대해 json()을 해줘야 하는 것이다(axios는 json()과정을 자동으로 해주는 셈이다).
+    */
+};
---
+++ study/onboarding-10-fe-p4/client/src/components/Sidebar.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { SidebarElement } from "../types/sidebar";
+import { useRouter } from "../hooks/useRouter";
+import { logout } from "../api/login";
+import { AdminRole, User } from "../types/user";
+import { useRecoilValue } from "recoil";
+import { UserAtom } from "../atoms/user";
+
+interface SidebarProps {
+    sidebarContent: SidebarElement[];
+    // TODO 4-2: Recoil atom `UserAtom`을 이용해 userProfile 값 대체 및 props type 삭제
+    userProfile: User | null;
+}
+
+// TODO 4-2: Recoil atom `UserAtom`을 이용해 userProfile 값 대체 및 props 삭제
+const Sidebar: React.FC<SidebarProps> = ({ sidebarContent, userProfile }) => {
+    // console.log(sidebarContent);
+    // (4) [{…}, {…}, {…}, {…}]
+    // 0: {id: 2, path: '/page-a', label: '페이지 A', isAdminOnly: false}
+    // 1: {id: 3, path: '/page-b', label: '페이지 B', isAdminOnly: false}
+    // 2: {id: 4, path: '/page-c', label: '페이지 C', isAdminOnly: false}
+    // 3: {id: 5, path: '/admin', label: 'Admin', isAdminOnly: true}
+
+    // console.log(userProfile);
+    // {userId: 0, username: 'blue', userInfo: {name: "blueStragglr", roles: ['user', 'admin']}}
+
+    // TODO 4-2: Recoil atom `UserAtom`을 이용해 userProfile 값 집어넣기
+    // hint: useRecoilValue
+    const { currentPath, routeTo } = useRouter();
+
+    const sidebarMenuClickHandler = (path: string) => {
+        routeTo(path);
+    };
+
+    // TODO 4-2: 로그아웃 호출
+    const logoutHandler = async () => {
+        await logout();
+        routeTo("/");
+    };
+
+    return (
+        <div className="sidebar">
+            <h3 className="sidebar-title">실습 3</h3>
+            <ul>
+                {sidebarContent
+                    .filter((element) => {
+                        // TODO 4-1: element.isAdminOnly가 true일 때
+                        //  admin user(userProfile.userInfo.roles에 'admin'이 포함됨)에게만 메뉴 보여주기
+                        return element.isAdminOnly
+                            ? userProfile?.userInfo.roles.includes(AdminRole)
+                            : element;
+                    })
+                    .map((element) => {
+                        return (
+                            <li
+                                key={element.path}
+                                className={
+                                    currentPath === element.path
+                                        ? "sidebar-menu selected"
+                                        : "sidebar-menu"
+                                }
+                                onClick={() =>
+                                    sidebarMenuClickHandler(element.path)
+                                }
+                            >
+                                {element.label}
+                            </li>
+                        );
+                    })}
+            </ul>
+            <div>
+                {userProfile ? (
+                    <div className="sidebar-footer">
+                        {userProfile?.userInfo?.name}
+                        {userProfile?.userInfo?.roles.includes(AdminRole)
+                            ? "(admin)"
+                            : ""}
+                        님 환영합니다.
+                        <button className={"small"} onClick={logoutHandler}>
+                            로그아웃
+                        </button>
+                    </div>
+                ) : (
+                    <div>로그인이 필요합니다.</div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;

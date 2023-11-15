68a7f14625fe76ce930afddfe34f41cd98dd33cb
be-brain <chha5232@gmail.com>
2023-11-15 13:30:13 +0900

    feat : Query resolvers(Graphql)

--- study/graphql/server.js
+++ study/graphql/server.js
@@ -1,6 +1,5 @@
 // package.json에서 type을 module로 설정하는 경우 ↓
 import { ApolloServer, gql } from "apollo-server";
-
 // package.json에서 type을 module로 설정하지 않는 경우 ↓
 // const { ApolloServer, gql } = require("apollo-server");
 
@@ -15,13 +14,14 @@ const typeDefs = gql`
     type Post {
         id: ID!
         text: String!
-        author: User!
+        author: User
     }
     # Query = REST api의 GET request
     # Mutation = REST api의 POST, PUT, DELETE request
     type Query {
         allPosts: [Post!]!
         post(id: ID!): Post
+        ping: String
     }
     # ! = required(non-nullable)
     # allPosts: [Post!]! = ① allPosts는 무조건 하나이상의 list를 주며([]!) ② 그 list는 항상 Post로 이루어져 있다(Post!)
@@ -33,7 +33,37 @@ const typeDefs = gql`
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const posts = [
+    {
+        id: "1",
+        text: "first",
+    },
+    {
+        id: "2",
+        text: "second",
+    },
+    {
+        id: "3",
+        text: "third",
+    },
+];
+
+// Server가 resolvers 함수를 부를때는 항상 arguments(① root, ② args: query/mutation에서 유저가 보낸 매개변수)를 전달한다
+const resolvers = {
+    Query: {
+        allPosts() {
+            return posts;
+        },
+        post(root, args) {
+            console.log(root, args);
+            return null;
+        },
+        ping() {
+            return "pong";
+        },
+    },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
 });

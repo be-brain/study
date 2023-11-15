49c6bcd18f2aa4bc48436ebda8adf45cfb27113e
be-brain <chha5232@gmail.com>
2023-11-15 10:47:02 +0900

    feat : non-nullable fields(Graphql)

--- study/graphql/server.js
+++ study/graphql/server.js
@@ -6,9 +6,7 @@ import { ApolloServer, gql } from "apollo-server";
 
 // Error: Apollo Server requires either an existing schema, modules or typeDefs
 // 사용자가 request할 모든 data들은 type Query안에 있어야 한다
-// Query = REST api의 GET request
 // allPosts = field
-// Mutation = REST api의 POST, PUT, DELETE request
 const typeDefs = gql`
     type User {
         id: ID
@@ -19,10 +17,16 @@ const typeDefs = gql`
         text: String!
         author: User!
     }
+    # Query = REST api의 GET request
+    # Mutation = REST api의 POST, PUT, DELETE request
     type Query {
         allPosts: [Post!]!
-        post(id: ID!): Post!
+        post(id: ID!): Post
     }
+    # ! = required(non-nullable)
+    # allPosts: [Post!]! = ① allPosts는 무조건 하나이상의 list를 주며([]!) ② 그 list는 항상 Post로 이루어져 있다(Post!)
+    # post(id: ID): Post = nullable field
+    # post(id: ID!): Post! = id(argument) 필수이며 항상 Post를 return해야한다
     type Mutation {
         createPost(text: String!, userId: ID!): Post!
         deletePost(id: ID!): Boolean!

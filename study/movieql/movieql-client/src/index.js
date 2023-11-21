c0c2788cb8f429230ac4737e340369a0d1a576eb
be-brain <chha5232@gmail.com>
2023-11-22 02:05:47 +0900

    feat : ApolloProvider

--- study/movieql/movieql-client/src/index.js
+++ study/movieql/movieql-client/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import client from "./client";
+import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        {/* ApolloProvider : 어떤 컴포넌트이던 client에 접근가능하게 해준다 */}
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>
 );
--- study/movieql/movieql-client/src/routes/Movies.js
+++ study/movieql/movieql-client/src/routes/Movies.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import gql from "graphql-tag";
+import { useApolloClient } from "@apollo/client";
 
 const Movies = () => {
-    return <div>Movies</div>;
+    const [movies, setMovies] = useState([]);
+    const client = useApolloClient();
+    useEffect(() => {
+        client
+            .query({
+                query: gql`
+                    {
+                        allMovies {
+                            id
+                            title
+                        }
+                    }
+                `,
+            })
+            .then((data) => setMovies(data.data.allMovies));
+    }, [client]);
+
+    return (
+        <div>
+            {movies.map((movie) => {
+                return <li key={movie.id}>{movie.title}</li>;
+            })}
+        </div>
+    );
 };
 
 export default Movies;

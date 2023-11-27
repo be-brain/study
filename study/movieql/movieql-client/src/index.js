c24bb2ecc83fc3ecf0a4c874f61b4771f48224de
be-brain <chha5232@gmail.com>
2023-11-28 07:42:55 +0900

    design : sass, styled-component 추가

--- study/movieql/movieql-client/src/index.js
+++ study/movieql/movieql-client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import client from "./client";
 import { ApolloProvider } from "@apollo/client";
+import { StyleSheetManager } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         {/* ApolloProvider : 어떤 컴포넌트이던 client에 접근가능하게 해준다 */}
         <ApolloProvider client={client}>
-            <App />
+            <StyleSheetManager shouldForwardProp={() => true}>
+                <App />
+            </StyleSheetManager>
         </ApolloProvider>
     </React.StrictMode>
 );
--- study/movieql/movieql-client/src/routes/Movies.js
+++ study/movieql/movieql-client/src/routes/Movies.js
@@ -2,16 +2,14 @@ import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_ALL_MOVIES = gql`
     query getAllMovies {
         allMovies {
             id
             title
-        }
-        allUsers {
-            id
-            fullName
+            poster_path
         }
     }
 `;
@@ -40,22 +38,83 @@ const Movies = () => {
 
     return (
         <>
-            {loading && <h1>Loading...</h1>}
-            {error && <h1>Something Wrong!</h1>}
-            <h3>Movie list</h3>
-            {data?.allMovies.map((movie) => {
-                return (
-                    <li key={movie.id}>
-                        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-                    </li>
-                );
-            })}
-            <h3>User list</h3>
-            {data?.allUsers.map((user) => {
-                return <li key={user.id}>{user.fullName}</li>;
-            })}
+            <Container>
+                <Header>
+                    <Title>Apollo Movies</Title>
+                </Header>
+                {error && <h1>Something Wrong!</h1>}
+                {loading && <Loading>Loading...</Loading>}
+                <MoviesGrid>
+                    {data?.allMovies?.map((movie) => (
+                        <PosterContainer key={movie.id}>
+                            <Link to={`/movies/${movie.id}`}>
+                                <PosterBg
+                                    background={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                                />
+                            </Link>
+                        </PosterContainer>
+                    ))}
+                </MoviesGrid>
+            </Container>
         </>
     );
 };
 
 export default Movies;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`;
+
+const Header = styled.header`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
+`;
+
+const PosterContainer = styled.div`
+    height: 400px;
+    border-radius: 7px;
+    width: 100%;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+    background-image: url(${(props) => props.background});
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    background-position: center center;
+    border-radius: 7px;
+`;

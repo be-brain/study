95e8c8067e80c936d31d4aab0fede27d10074478
be-brain <chha5232@gmail.com>
2023-12-05 06:00:57 +0900

    feat : sass추가, 여러 html파일들로 url추가하기

--- study/Nodejs/TodoApp_node/server.js
+++ study/Nodejs/TodoApp_node/server.js
@@ -4,6 +4,8 @@ const app = express();
 app.listen(8080, function () {
     console.log("Server Open : 8080");
 });
+// public폴더 내 파일들 index.html과 연결
+app.use(express.static(__dirname + "/public"));
 
 /**
  * "/start"로 접속시 시작화면 보여주기
@@ -11,7 +13,15 @@ app.listen(8080, function () {
  *  response.send("시작화면")
  * })
  */
-
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+app.get("/goal", function (req, res) {
+    res.sendFile(__dirname + "/goal.html");
+});
+app.get("/done", function (req, res) {
+    res.sendFile(__dirname + "/done.html");
+});
 app.get("/start", function (req, res) {
     res.send("시작화면입니다");
 });

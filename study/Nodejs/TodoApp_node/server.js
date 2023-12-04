0771d9f4e84dfb25c80b6aa707afe72e50ea83c6
be-brain <chha5232@gmail.com>
2023-12-05 08:40:04 +0900

    feat : mongodb설치, 연결 및 dotenv설정

--- study/Nodejs/TodoApp_node/server.js
+++ study/Nodejs/TodoApp_node/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, function () {
-    console.log("Server Open : 8080");
-});
 // public폴더 내 파일들 index.html과 연결
 app.use(express.static(__dirname + "/public"));
+
+// mongodb연결
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+let db;
+const url =
+    "mongodb+srv://" +
+    process.env.MONGODB_ID +
+    encodeURIComponent(process.env.MONGODB_PW) +
+    "@cluster0.czwqqpl.mongodb.net/?retryWrites=true&w=majority";
+
+new MongoClient(url)
+    .connect()
+    .then((client) => {
+        console.log("mongodb connecting success");
+        db = client.db("node-board");
+        app.listen(8080, function () {
+            console.log("Server Open : http://localhost:8080");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 /**
  * "/start"로 접속시 시작화면 보여주기
@@ -17,6 +38,7 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 app.get("/goal", function (req, res) {
+    db.collection("post").insertOne({ title: "test1" });
     res.sendFile(__dirname + "/goal.html");
 });
 app.get("/done", function (req, res) {

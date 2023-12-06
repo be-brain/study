6f038886c8279f1120c8a38467d0e9ef7ad47593
be-brain <chha5232@gmail.com>
2023-12-06 20:30:53 +0900

    feat : ejs설치, server-side-rendering

--- study/Nodejs/TodoApp_node/server.js
+++ study/Nodejs/TodoApp_node/server.js
@@ -4,6 +4,14 @@ const app = express();
 // public폴더 내 파일들 index.html과 연결
 app.use(express.static(__dirname + "/public"));
 
+/**
+ * web template engine : 템플릿 양식과 데이터를 합쳐 HTML 문서를 출력하는 소프트웨어.
+ * 서버사이드 템플릿엔진 : 서버에서 DB 혹은 API에서 가져온 데이터를 미리 정의된 Template에 넣어 Html을 그려서 클라이언트에 전달해주는 역할
+ * 클라이언트사이드 템플릿엔진 : html 형태로 코드를 작성할 수 있으며, 동적으로 DOM을 그리게 해주는 역할
+ */
+// template engine(ejs)와 연결
+app.set("view engine", "ejs");
+
 // mongodb연결
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -46,5 +54,8 @@ app.get("/done", function (req, res) {
 });
 app.get("/post", async (req, res) => {
     const result = await db.collection("post").find().toArray();
-    res.send(result);
+    res.render("myPost.ejs", { data: result }); // 응답은 1번만 가능하다(뒤에 res.send 몇개를 쓰던 맨 위 1개만 실행됨)
+});
+app.get("/time", (req, res) => {
+    res.render("currentTime.ejs", { time: new Date() });
 });
